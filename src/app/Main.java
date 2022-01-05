947ef1a89e4ce144189dcd06f96d4365755441c8
rozkosna <lenka.rozkosna@gjkt.eu>
2022-01-05 21:33:25 +0100

    SceneBuilder1

--- src/app/Main.java
+++ src/app/Main.java
@@ -14,9 +14,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/sample.fxml"));
         primaryStage.setTitle("ČLOVĚČE NEZLOB SE");
         primaryStage.setScene(new Scene(root));
-
+        primaryStage.show();
     }
 }
--- src/view/Controller.java
+++ src/view/Controller.java
@@ -1,4 +1,49 @@
 package view;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import model.Kostka;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    Button kostka;
+
+    @FXML
+    Label hodKostkou;
+
+    ArrayList<Position> poleSouradnic = new ArrayList();
+
+    ArrayList<Circle> svetylka;
+
+    int hod = 0;
+
+    @FXML
+    AnchorPane pozadi;
+
+    public void hodKostkou(){
+        Kostka kostka = new Kostka(6);
+        hod = kostka.hodKostkou();
+        hodKostkou.setText(String.valueOf(hod));
+//        hodKostkou.setLayoutX(300);
+//        pozadi.getChildren().add(hodKostkou);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        generujSouradnice();
+    }
+
+    private void generujSouradnice() {
+
+        //poleSouradnic.add(new Position(x,y));
+    }
 }
